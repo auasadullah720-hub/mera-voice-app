0eb928d1f61faa83d52c46f0d8ba5dc9bbe09619
auasadullah720-hub <auasadullah720@gmail.com>
2026-05-13 00:50:13 +0500

    Update App.py

--- App.py
+++ App.py
@@ -1,14 +1,19 @@
 import streamlit as st
-from gtts import gTTS
+import asyncio
+import edge_tts
 import os
 
-st.title("Saifullah Bhai ka Voice App")
-user_text = st.text_input("Yahan text likhein:", "G kon bol raha hai?")
+st.title("Saifullah Bhai ka Super Voice App")
 
-if st.button("Awaaz Banayein"):
+user_text = st.text_input("Yahan text likhein:", "Assalam-o-Alaikum, kaise hain aap?")
+
+async def generate_voice(text):
+    voice = "ur-PK-FatimaNeural" 
+    communicate = edge_tts.Communicate(text, voice)
+    await communicate.save("voice.mp3")
+
+if st.button("Behtareen Awaaz Banayein"):
     if user_text:
-        tts = gTTS(text=user_text, lang='hi', slow=False)
-        tts.save("voice.mp3")
-        audio_file = open("voice.mp3", "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mp3")
+        with st.spinner('Awaaz ban rahi hai...'):
+            asyncio.run(generate_voice(user_text))
+            st.audio("voice.mp3")
